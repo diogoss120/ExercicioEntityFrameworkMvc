be18be90a801f7a135fdb3e26e095b7e8cc9c23d
Diogo Souza <ddiiogo.souza25710@hotmail.com>
2021-05-12 17:48:30 -0300

    acesso ao mysql configurado

---
+++ AlunoEFCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlunoTeste.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

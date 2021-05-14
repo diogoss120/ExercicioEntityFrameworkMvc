6eb0867fd7c2744207cef8046c031db9f97f3c73
Diogo Souza <ddiiogo.souza25710@hotmail.com>
2021-05-14 13:59:50 -0300

    configurando para rodar no servidor IIS

--- AlunoEFCore/Program.cs
+++ AlunoEFCore/Program.cs
@@ -20,7 +20,8 @@ namespace AlunoTeste
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseIISIntegration();
                 });
     }
 }

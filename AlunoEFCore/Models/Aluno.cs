56b96b56fb9cc240549bcde6dd3cedb3fc5ca1c6
Diogo Souza <ddiiogo.souza25710@hotmail.com>
2021-05-14 09:24:32 -0300

    primeira migration do EFCore para o mysql

--- AlunoEFCore/Models/Aluno.cs
+++ AlunoEFCore/Models/Aluno.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,25 @@ namespace AlunoTeste.Models
     public class Aluno
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome deve ser informado")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "O nome deve ter no mínimo 3 caracteres")]
         public string Nome { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "O Sexo deve ser informado")]
         public string Sexo { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "O Email deve ser informado")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
         public string Email { get; set; }
+
+        [Display(Name = "Nascimento")]
+        [Required(ErrorMessage = "Informe a data de nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Nascimento { get; set; }
     }
 }
